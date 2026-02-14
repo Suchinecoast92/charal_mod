f914465de61872a54d6971f0020de751229265e7
Suchinecoast92 <moises03_93@hotmail.com>
2026-02-14 10:23:48 -0600

    cambio de version 1.20.1

---
+++ src/client/kotlin/charal_mod/Charal_modClient.kt
@@ -0,0 +1,9 @@
+package charal_mod
+
+import net.fabricmc.api.ClientModInitializer
+
+object Charal_modClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/charal_mod/Charal_mod.kt
@@ -0,0 +1,15 @@
+package charal_mod
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Charal_mod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("charal_mod")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
